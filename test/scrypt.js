da368649eb8f937ca0bf820d39aa3e8a971155a6
GodwinCT <104574193+GodwinCT@users.noreply.github.com>
2023-03-23 09:08:32 +0200

    first commit

---
+++ test/scrypt.js
@@ -0,0 +1,12 @@
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,  // x coordinate
+        y: 1   // y coordinate
+    }
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+ci
